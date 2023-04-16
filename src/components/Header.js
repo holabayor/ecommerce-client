8b9ee32fb60b5969ea804fc426c6b39e3af05b48
holabayor <liasu.olabayo@gmail.com>
2023-04-16 08:39:50 +0100

    header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,57 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { BsSearch } from 'react-icons/bs';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <header className="header-top-strip py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <p className="text-white mb-0">
+                Free Shipping Over ₦20,000 & Free Returns
+              </p>
+            </div>
+            <div className="col-6">
+              <p className="text-end text-white mb-o">
+                Hotline:{' '}
+                <a className="text-white" href="tel:2348140555831">
+                  +234 814 055 5831
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className="header-upper py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-2">
+              <h1>
+                <Link className="text-white">Developers</Link>
+              </h1>
+            </div>
+            <div className="col-5">
+              <div className="input-group mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Seacrh Product Here..."
+                  aria-label="Seacrh Product Here..."
+                  aria-describedby="basic-addon2"
+                />
+                <span className="input-group-text" id="basic-addon2">
+                  <BsSearch />
+                </span>
+              </div>
+            </div>
+            <div className="col-5"></div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Header;
